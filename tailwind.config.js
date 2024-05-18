947c583b2407ea99f32243bc6c6b6934a2bb33c6
Kofiastro <astrokofi@gmail.com>
2024-05-18 18:09:17 +0000

    new starter

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,27 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    screens:{
-      
+    screens: {
+      sm: '375px',
+      md: "1440px",
+
     },
-    extend: {},
+    extend: {
+      colors: {
+        Nutmeg: 'hsl(14, 45%, 36%)',
+        DarkRaspberry: 'hsl(332, 51%, 32%)',
+        White: 'hsl(0, 0%, 100%)',
+        RoseWhite: 'hsl(330, 100%, 98%)',
+        Eggshell: 'hsl(30, 54%, 90%)',
+        LightGrey: 'hsl(30, 18%, 87%)',
+        WengeBrown: 'hsl(30, 10%, 34%)',
+        DarkCharcoal: 'hsl(24, 5%, 18%)'
+      },
+      fontFamily:{
+        sans:['Young Serif', 'serif'],
+        
+      }
+    },
   },
   plugins: [],
 }
